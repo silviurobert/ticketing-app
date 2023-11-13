953fc49853c259b4b3cbeed6e80c08e8bb5c7c46
silviurobert <silviusocial@gmail.com>
2023-11-13 19:44:44 +0200

    Add location class

---
+++ Project/Location.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <vector>
+
+class Location
+{
+private:
+    int maxNumberOfSeats = 0;
+    int numberOfRows = 0;
+    int seatsPerRow = 0;
+    //attribute zone?
+
+public: 
+    int getNumberOfSeats() {
+        return this->maxNumberOfSeats;
+    }
+
+    void setNumberOfSeats(int value) {
+        this->maxNumberOfSeats = value;
+    }
+
+    int getNumberOfRows() {
+        return this->numberOfRows;
+    }
+
+    void setNumberOfRows(int value) {
+        this->numberOfRows = value;
+    }
+
+    int getSeatsPerRow() {
+        return this->seatsPerRow;
+    }
+
+    void setSeatsPerRow(int value) {
+        this->seatsPerRow = value;
+    }
+
+    Location(int maxNumberOfSeats, int numberOfRows, int seatsPerRow) :
+        maxNumberOfSeats(maxNumberOfSeats), numberOfRows(numberOfRows), seatsPerRow(seatsPerRow) {
+
+    }
+
+
+    
+};
+
+
--- Project/Main.cpp
+++ Project/Main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
+#include "Location.h"
 
 int main() {
-	cout << "Hello world!";
+
+	Location location1(2000, 100, 20);
+
+	cout << endl << "2000, 100, 20";
+	cout << endl << "The maximum number of seats is: " << location1.getNumberOfSeats();
+	cout << endl << "The number of rows is: " << location1.getNumberOfRows();
+	cout << endl << "The number of seats per row is: " << location1.getSeatsPerRow();
+	
 }
